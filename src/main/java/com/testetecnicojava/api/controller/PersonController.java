a7b6a58c1e8160f4092f00c837ed7e8a50c8cba6
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2023-01-18 01:41:08 -0300

    feat: add person to database

--- src/main/java/com/testetecnicojava/api/controller/PersonController.java
+++ src/main/java/com/testetecnicojava/api/controller/PersonController.java
@@ -1,12 +1,16 @@
 package com.testetecnicojava.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.testetecnicojava.api.dto.PersonDTO;
+import com.testetecnicojava.api.model.Person;
 import com.testetecnicojava.api.repository.PersonRepository;
 
 @RestController
@@ -18,6 +22,11 @@ public class PersonController {
 
   @PostMapping
   public void create(@RequestBody PersonDTO req){
-    
+    repository.save(new Person(req));
+  }
+
+  @GetMapping
+  public List<Person> findAll(){
+    return repository.findAll();
   }
 }
