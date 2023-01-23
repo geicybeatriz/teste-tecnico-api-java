11c060458247d84e94c074787a66a5c019070682
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2023-01-23 01:41:25 -0300

    "update application tests"

--- src/test/java/com/testetecnicojava/api/ApiApplicationTests.java
+++ src/test/java/com/testetecnicojava/api/ApiApplicationTests.java
@@ -1,30 +1,13 @@
 package com.testetecnicojava.api;
 
-
-//import java.util.Date;
-
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-//import com.testetecnicojava.api.dto.PersonDTO;
-//import com.testetecnicojava.api.services.PersonServices;
 
 @SpringBootTest
 class ApiApplicationTests {
 
-	//PersonDTO personTest = new PersonDTO("Geicy Beatriz", "1995-06-02");
-
 	@Test
-	@DisplayName("Deve cadastrar uma pessoa")
 	void createPerson() {
-		//PersonServices personServices = new PersonServices();
-		//acho que terei de usar mockito
 		
-		//personServices.create(personTest);
-
-		
-
 	}
-
 }
